e5517b1ee18f5433ceef806c6747af0ee932ff4f
PaulStuderegger <paul.studeregger@gmail.com>
2023-05-03 14:44:11 +0200

    index.php initial commit

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rainbow Text</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/css/bootstrap.min.css">
+  <style>
+    .rainbow-text {
+      background: linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1 class="rainbow-text">Hello everyone!</h1>
+  </div>
+</body>
+</html>
