931bb299bd64f742b9c2db5021c68609ff020430
PaulStuderegger <paul.studeregger@gmail.com>
2023-05-10 15:55:02 +0200

    db class exception handling

--- classes/db.php
+++ classes/db.php
@@ -6,13 +6,17 @@ class Database
     protected $db_user = "root";
     protected $db_password = "";
     public $pdo;
-    
+
     // create connection
     public function __construct()
     {
-        $pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_password);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo = $pdo;
+        try {
+            $pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_password);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+        } catch (PDOException $pe){
+            echo $pe->getMessage();
+        }
     }
 }
