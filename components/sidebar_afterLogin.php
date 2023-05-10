ad860861545658e6a789d0c6228460dd4f8b368c
Matthias Weingartner <toothless21799@gmail.com>
2023-05-10 15:06:48 +0200

    Grunddesign erstellen

---
+++ components/sidebar_afterLogin.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+    <h2>MyConomy</h2>
+    <ul>
+        <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
+        <li><a href="#"><i class="fas fa-hashtag"></i>Function 1</a></li>
+        <li><a href="#"><i class="fas fa-hashtag"></i>Function 2</a></li>
+        <li><a href="#"><i class="fas fa-hashtag"></i>Function 3</a></li>
+        <li><a href="#"><i class="fas fa-hashtag"></i>Function 4</a></li>
+        <li><a href="#"><i class="fas fa-hashtag"></i>Function 5</a></li>
+        <li><a href="#"><i class="fas fa-user"></i>Abmelden</a></li>
+    </ul> 
+    <div class="social_media">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+</div>
---
+++ components/sidebar_beforeLogin.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <h2>MyConomy</h2>
+    <ul>
+        <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
+        <li><a href="#"><i class="fas fa-user"></i>Login</a></li>
+    </ul> 
+    <div class="social_media">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+</div>
