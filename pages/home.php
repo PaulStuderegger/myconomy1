4f3d3acd3a66ec9aad9b9457e9d4efa5e533e278
Tom Weis <tomleon.weis@gmail.com>
2023-05-11 08:24:38 +0200

    navbar/home update

--- pages/home.php
+++ pages/home.php
@@ -4,9 +4,12 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-<?php
-include('../components/sidebar_afterLogin.php');
-include('../components/navbar.php');
-?>
+  <div class="sidebar">
+<div class="wrapper">
+<?php include('../components/sidebar_afterLogin.php'); ?>
+</div>
+</div>
+<?php include('../components/navbar.php'); ?>
+
 </body>
 </html>
