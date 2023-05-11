5e8fe17d6e0ccf9b1ce522266ae5f38d9f39c5ac
PaulStuderegger <paul.studeregger@gmail.com>
2023-05-11 15:25:30 +0200

    fix login

--- components/sidebar_beforeLogin.php
+++ components/sidebar_beforeLogin.php
@@ -2,6 +2,11 @@
     <h2>MyConomy</h2>
     <ul>
         <li><a href="login.php"><i class="fas fa-user"></i>Login</a></li>
+        <li>
+            <form class="fas fa-user" action="https://youtu.be/dQw4w9WgXcQ">
+                <input type="submit" value="🥵" />
+            </form>
+        </li>
     </ul> 
     <div class="social_media">
         <a href="#"><i class="fab fa-facebook-f"></i></a>
