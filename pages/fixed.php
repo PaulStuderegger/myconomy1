62be41ffbe7a710407b22fa2bedaa7328d580e97
Tom Weis <tomleon.weis@gmail.com>
2023-05-12 09:44:35 +0200

    update "fixkosten" form

--- pages/fixed.php
+++ pages/fixed.php
@@ -1,9 +1,33 @@
-<h1> hier kommen die fixkosten hin </h1>
+<h1>Fixkosten</h1>
 
 <button id="new_entry">neuer Eintrag</button>
 <form id="new_entry_form" action="../forms/fixed_action.php" method="post">
-  <label for="test">Test</label>
-  <input type="text" placeholder="text">
-  <input type="reset" value="abbrechen">
+  
+<legend>Fixkosten: Eintrag hinzufügen</legend>
+  <input type="text" placeholder="Verwendungszweck">
+  <input type="number" placeholder="Betrag">
+  <select id="category" name="category">
+  <option value="none">(bitte auswählen)</option>  
+  <option value="verkehr">Verkehr</option>
+    <option value="kommunikation">Handy/Internet</option>
+    <option value="unterhaltung">Unterhaltung (Streaming)</option>
+    <option value="miete">Miete</option>
+    <option value="sonstiges">Sonstiges</option>
+</select>
+<div>  
+  <input type="reset" id="reset" value="zurücksetzen">
+<input type="button" id="cancel" value="abbrechen">
   <input type="submit" value="speichern">
+</div>
 </form>
+
+<h2> hier kommen die fixkosten hin </h2>
+
+<table>
+<tr>
+  <th>Bezeichnung</th>
+  <th>Kategorie</th>
+  <th>Betrag</th>
+</tr>
+<tr><td>hier dynamische tabelle</td></tr>
+</table>
--- scripts/fixed.js
+++ scripts/fixed.js
@@ -1,8 +1,11 @@
 window.onload = function() {
   let new_entry = document.getElementById("new_entry");
+  let new_entry_cancel = document.getElementById("cancel");
   let new_entry_form = document.getElementById("new_entry_form");
   new_entry.addEventListener("click", function () {
     new_entry_form.style.display = "block";
-    console.log("form activated");
+  });
+  new_entry_cancel.addEventListener("click", function () {
+    new_entry_form.style.display = "none";
   });
 };
