0aacfbbe330198869220a2f52c93b2d3b04d9b64
PaulStuderegger <paul.studeregger@gmail.com>
2023-05-15 15:15:47 +0200

    password security B-)

--- classes/user.php
+++ classes/user.php
@@ -20,33 +20,37 @@ class User extends Database
     
 	public function InsertUserToDB()
 	{
-		// $this->Password = password_hash($this->Password, PASSWORD_DEFAULT);
+		$hashedPassword = password_hash($this->Password, PASSWORD_DEFAULT);
         $stmt = $this->pdo->prepare("INSERT INTO User(UserName, EMail, Password) VALUES (?,?,?)");
-		$stmt->execute([$this->UserName, $this->EMail, $this->Password]);
+		$stmt->execute([$this->UserName, $this->EMail, $hashedPassword]);
 	}
 
-	// Evt. Hashen von Passwörtern
     public static function ValidateUser($EMail, $Password)
 	{
 		$db = new Database();
-		$stmt = $db->pdo->prepare("SELECT * FROM User WHERE EMail = ? AND Password = ?");
-		$stmt->execute([$EMail, $Password]);
+		$stmt = $db->pdo->prepare("SELECT * FROM User WHERE EMail = ?");
+		$stmt->execute([$EMail]);
 		$res = $stmt->fetch();
 
 		if ($res) {
 			$UserData = new User($res["UserId"], $res["UserName"], $res["EMail"], $res["Password"]);
-		
-			$_SESSION['logged'] = true;                                                        
-			$_SESSION['loggedUser'] = array(
-				"UserId"=>$UserData->UserId,
-				"UserName"=>$UserData->UserName,
-				"EMail"=>$UserData->EMail,
-				"Password"=>$UserData->Password
-			);
-			header ('Location: home.php');
+
+			if (password_verify($Password, $res["Password"])) {
+				$_SESSION['logged'] = true;                                                        
+				$_SESSION['loggedUser'] = array(
+					"UserId"=>$UserData->UserId,
+					"UserName"=>$UserData->UserName,
+					"EMail"=>$UserData->EMail,
+					"Password"=>$UserData->Password
+				);
+				header ('Location: home.php');
+			}
+			else {
+				echo '<br><div class="alert alert-danger" role="alert">Wrong password</div>';
+			}
 		}
 		else {
-			echo '<br><div class="alert alert-danger" role="alert">Wrong username or password.</div>';
+			echo '<br><div class="alert alert-danger" role="alert">username does not exist</div>';
 		}
 	}
 	
