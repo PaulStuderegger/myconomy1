daa32d9c3c3e5628a83a624d58b66d3fa364eee0
PaulStuderegger <paul.studeregger@gmail.com>
2023-05-15 10:33:01 +0200

    minor changes + function to add balance and calender

--- classes/user.php
+++ classes/user.php
@@ -1,26 +1,32 @@
 <?php
 require_once "db.php";
+require_once "balance.php";
+require_once "utils.php";
 class User extends Database
 {
 	public $UserId;
 	public $UserName;
 	public $EMail;
 	public $Password;
+	public $BalanceId;
+	public $CalenderId;
 
-	function __construct($UserId = null, $UserName = null, $EMail = null, $Password = null)
+	function __construct($UserId = null, $UserName = null, $EMail = null, $Password = null, $BalanceId = null, $CalenderId = null)
 	{
 		parent::__construct();
 		$this->UserId = $UserId;
 		$this->UserName = $UserName;
 		$this->EMail = $EMail;
 		$this->Password = $Password;
+		$this->BalanceId = $BalanceId;
+		$this->CalenderId = $CalenderId;
 	}
     
 	public function InsertUserToDB()
 	{
 		// $this->Password = password_hash($this->Password, PASSWORD_DEFAULT);
-        $stmt = $this->pdo->prepare("INSERT INTO User(UserName, EMail, Password) VALUES (?,?,?)");
-		$stmt->execute([$this->UserName, $this->EMail, $this->Password]);
+        $stmt = $this->pdo->prepare("INSERT INTO User(UserName, EMail, Password, BalanceId, CalenderId) VALUES (?,?,?,?,?)");
+		$stmt->execute([$this->UserName, $this->EMail, $this->Password, $this->BalanceId, $this->CalenderId]);
 	}
 
 	// Evt. Hashen von Passwörtern
@@ -32,19 +38,38 @@ class User extends Database
 		$res = $stmt->fetch();
 
 		if ($res) {
-			$UserData = new User($res["UserId"], $res["UserName"], $res["EMail"], $res["Password"]);
+			$UserData = new User($res["UserId"], $res["UserName"], $res["EMail"], $res["Password"], $res["BalanceId"]);
 		
 			$_SESSION['logged'] = true;                                                        
 			$_SESSION['loggedUser'] = array(
 				"UserId"=>$UserData->UserId,
 				"UserName"=>$UserData->UserName,
 				"EMail"=>$UserData->EMail,
-				"Passwors"=>$UserData->Password
+				"Password"=>$UserData->Password,
+				"BalanceId"=>$UserData->BalanceId
 			);
 			header ('Location: home.php');
 		}
 		else {
 			echo '<br><div class="alert alert-danger" role="alert">Wrong username or password.</div>';
 		}
+	}
+	
+	public function ConnectUserToBalance()
+	{
+		$balance = new Balance(Utils::nextId("Balance"), 0.0, date("d-m-y"));
+		$balance->InsertBalanceToDB();
+		
+        $stmt = $this->pdo->prepare("UPDATE User SET BalanceId = ? WHERE UserId = ?");
+		$stmt->execute([$balance->BalanceId, $this->UserId]);
+	}
+	
+	public function ConnectUserToCalender()
+	{
+		$calender = new Calender(Utils::nextId("Calender"), "Kalender");
+		$calender->InsertCalenderToDB();
+		
+        $stmt = $this->pdo->prepare("UPDATE User SET CalenderId = ? WHERE UserId = ?");
+		$stmt->execute([$calender->CalenderId, $this->UserId]);
 	}
 }
