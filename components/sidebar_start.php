c56a77f4dc80312402841bba7f2dbb2264ccc2f8
SilverLining4869 <toothless21799@gmail.com>
2023-05-18 18:59:10 +0200

    sidebar erweitern

--- components/sidebar_start.php
+++ components/sidebar_start.php
@@ -3,13 +3,27 @@
       
     <div class="list-group list-group-flush my-3">
         <a href="?menu=funktionen" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-gift me-2"></i>Funktionen
+            <i class="fas fa-gift me-2"></i> Funktionen
         </a>
+
         <a href="?menu=signin" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-sign-in-alt me-2"></i>Login
+            <i class="fas fa-sign-in-alt me-2"></i> Anmelden
         </a>
+
         <!-- <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold">
             <i class="fas fa-power-off me-2"></i>Logout
         </a> -->
+
+        <a href="?menu=datenschutz" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-user-secret me-2"></i> Datenschutz
+        </a>
+
+        <a href="?menu=impressum" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-industry me-2"></i> Impressum
+        </a>
+
+        <a href="?menu=kontakt" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-envelope me-2"></i> Kontakt
+        </a>
     </div>
 </div>
