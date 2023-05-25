a584056d26e653ff5ae21325d4283690655404cb
Matthias Weingartner <toothless21799@gmail.com>
2023-05-25 08:21:17 +0200

    sidebar component pre login erstellen

---
+++ components/sidebar_pre_login.php
@@ -0,0 +1,27 @@
+<div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
+    <i class="fas fa-money-bill-wave me-2"></i>My-Conomy</div>
+      
+    <div class="list-group list-group-flush my-3">
+        <a href="?menu=funktionen" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-gift me-2"></i>Funktionen
+        </a>
+        <a href="?menu=signin" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-sign-in-alt me-2"></i>Login
+        </a>
+        <!-- <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold">
+            <i class="fas fa-power-off me-2"></i>Logout
+        </a> -->
+        <a href="?menu=kontakt" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-envelope me-2"></i>Kontakt
+        </a>
+        <a href="?menu=datenschutz" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-user-secret me-2"></i>Datenschutz
+        </a>
+        <a href="?menu=impressum" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-industry me-2"></i>Impressum
+        </a>
+        <a href="?menu=template" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            VORLAGE
+        </a>
+    </div>
+</div>
