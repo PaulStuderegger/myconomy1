9d763cbee41f6a5d02e302412fd885fc344801f6
Matthias Weingartner <toothless21799@gmail.com>
2023-05-26 08:13:35 +0200

    datenschutz nach template umbauen

--- components/datenschutz.php
+++ components/datenschutz.php
@@ -1,46 +1,71 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-  <meta charset="UTF-8">
-	<title>Datenschutz</title>
+<?php
+require_once "..\classes\user.php";
 
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-    
-    <!-- eigenes CSS -->
-    <link rel="stylesheet" href="../styles/styles.css" />
-    <link rel="stylesheet" href="../styles/login.css" />
-</head>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
 
-<body>
-  <div class="container-fluid px-4">
-    <div class="row g-3 my-2">
+  // Validate the login credentials
+  User::ValidateUserSignIn($username, $password);
+}
+?>
+
+<!-- header -->
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+  <div class="d-flex align-items-center">
+    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+    <h2 class="fs-2 m-0">Datenschutz</h2>
+  </div>
+
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+    aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
+          role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="fas fa-user me-2"></i>John Doe
+        </a>
+                
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#"><i class="fas fa-power-off me-2"></i>Abmelden</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<!-- main content -->
+<div class="container-fluid px-4">
+  <div class="row g-3 my-2">
           
-      <div class="col-md-4"></div>
+    <div class="col-md-4"></div>
             
-      <div class="col-md-4">
-        <div class="p-3 bg-white shadow-sm justify-content-around align-items-center rounded">
-          <div>
-            <h5>Datenschutzerklärung:</h5>
-            <p>
-                Diese Website ist aufgrund eines Schulprojektes entstanden.
-                <br /> Es ist möglich, dass Daten nicht entsprechend der DSGVO
-                gehandhabt werden, jedoch wird Ihr Passwort und Ihre E-Mail
-                Adresse sicher in unserer Datenbank gespeichert. <br />
-                Falls Sie Fragen zu der Handhabung der Daten haben, schreiben
-                Sie bitte eine E-Mail über das Kontaktformular. <br />
-                Wir garantieren jedoch nicht, dass Ihre Daten zu 100% gegen
-                &ldquo;Man-in-the-Middle-Angriffe&rdquo; geschützt sind,
-                jedoch arbeiten wir ständig daran die Sicherheit unserer
-                Website zu verbessern.
-            </p>  
-          </div>
+    <div class="col-md-4">
+      <div class="p-3 bg-white shadow-sm justify-content-around align-items-center rounded">
+        <div>
+          <h5>Datenschutzerklärung:</h5>
+          <p>
+              Diese Website ist aufgrund eines Schulprojektes entstanden.
+              <br /> Es ist möglich, dass Daten nicht entsprechend der DSGVO
+              gehandhabt werden, jedoch wird Ihr Passwort und Ihre E-Mail
+              Adresse sicher in unserer Datenbank gespeichert. <br />
+              Falls Sie Fragen zu der Handhabung der Daten haben, schreiben
+              Sie bitte eine E-Mail über das Kontaktformular. <br />
+              Wir garantieren jedoch nicht, dass Ihre Daten zu 100% gegen
+              &ldquo;Man-in-the-Middle-Angriffe&rdquo; geschützt sind,
+              jedoch arbeiten wir ständig daran die Sicherheit unserer
+              Website zu verbessern.
+          </p>  
         </div>
       </div>
-
-      <div class="col-md-4"></div>
     </div>
+
+    <div class="col-md-4"></div>
+
   </div>
-</body>
-</html>
+</div>
