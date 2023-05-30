8a9fd0bd825689d98d87598152e80dc4a9d8b1e3
PaulStuderegger <paul.studeregger@gmail.com>
2023-05-30 14:02:42 +0200

    added port parameter for db connect

--- classes/db.php
+++ classes/db.php
@@ -4,14 +4,15 @@ class Database
     protected $db_host = 'localhost';
     protected $db_name = "MyConomy"; // wird noch geändert
     protected $db_user = "root";
-    protected $db_password = "root";
+    protected $db_password = "";
+    protected $db_port = 3307;
     public $pdo;
 
     // create connection
     public function __construct()
     {
         try {
-            $pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_password);
+            $pdo = new PDO("mysql:host=$this->db_host;port=$this->db_port;dbname=$this->db_name", $this->db_user, $this->db_password);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
