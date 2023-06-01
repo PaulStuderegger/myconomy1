d208cb99b4853885c7d931bdc12edd6729257d2c
Matthias Weingartner <toothless21799@gmail.com>
2023-06-01 08:07:16 +0200

    sidebar_post_login erstellen

---
+++ components/sidebar_post_login.php
@@ -0,0 +1,33 @@
+<div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold border-bottom">
+    <i class="fas fa-money-bill-wave me-2"></i>MyConomy</div>
+      
+    <div class="list-group list-group-flush my-3">
+        <a href="?menu=funktionen" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-list me-2"></i>Funktionen
+        </a>
+        <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-balance-scale me-2"></i>Bilanz
+        </a>
+        <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-calendar-alt me-2"></i>Kalender
+        </a>
+        <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-chart-area me-2"></i>Prognose
+        </a>
+        <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-piggy-bank me-2"></i>Sparen
+        </a>
+
+        <div class="border-top">
+            <a href="?menu=kontakt" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                <i class="fas fa-envelope me-2"></i>Kontakt
+            </a>
+            <a href="?menu=datenschutz" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                <i class="fas fa-user-secret me-2"></i>Datenschutz
+            </a>
+            <a href="?menu=impressum" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                <i class="fas fa-industry me-2"></i>Impressum
+            </a>
+        </div>
+    </div>
+</div>
