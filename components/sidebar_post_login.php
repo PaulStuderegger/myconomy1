cb74ad375e26414f8be2c1281ed7bd2f66afa954
Matthias Weingartner <toothless21799@gmail.com>
2023-06-01 08:57:05 +0200

    sidebar files refactoren

--- components/sidebar_post_login.php
+++ components/sidebar_post_login.php
@@ -3,31 +3,36 @@
       
     <div class="list-group list-group-flush my-3">
         <a href="?menu=funktionen" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-list me-2"></i>Funktionen
+            <i class="fas fa-list me-2"></i>
+            Funktionen
         </a>
         <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-balance-scale me-2"></i>Bilanz
+            <i class="fas fa-balance-scale me-2"></i>
+            Bilanz
         </a>
         <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-calendar-alt me-2"></i>Kalender
+            <i class="fas fa-calendar-alt me-2"></i>
+            Kalender
         </a>
         <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-chart-area me-2"></i>Prognose
+            <i class="fas fa-chart-area me-2"></i>
+            Prognose
         </a>
         <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-piggy-bank me-2"></i>Sparen
+            <i class="fas fa-piggy-bank me-2"></i>
+            Sparen
         </a>
-
-        <div class="border-top">
-            <a href="?menu=kontakt" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                <i class="fas fa-envelope me-2"></i>Kontakt
-            </a>
-            <a href="?menu=datenschutz" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                <i class="fas fa-user-secret me-2"></i>Datenschutz
-            </a>
-            <a href="?menu=impressum" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                <i class="fas fa-industry me-2"></i>Impressum
-            </a>
-        </div>
+        <a href="?menu=kontakt" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-envelope me-2"></i>
+            Kontakt
+        </a>
+        <a href="?menu=datenschutz" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-user-secret me-2"></i>
+            Datenschutz
+        </a>
+        <a href="?menu=impressum" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i class="fas fa-industry me-2"></i>
+            Impressum
+        </a>
     </div>
 </div>
--- components/sidebar_pre_login.php
+++ components/sidebar_pre_login.php
@@ -1,31 +1,34 @@
-<div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
-    <i class="fas fa-money-bill-wave me-2"></i>My-Conomy</div>
+<div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold border-bottom">
+    <i class="fas fa-money-bill-wave me-2"></i>MyConomy</div>
       
     <div class="list-group list-group-flush my-3">
         <a href="?menu=funktionen" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-gift me-2"></i>Funktionen
+            <i class="fas fa-list me-2"></i>
+            Funktionen
         </a>
         <a href="?menu=signin" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-sign-in-alt me-2"></i>Login
+            <i class="fas fa-sign-in-alt me-2"></i>
+            Anmelden
         </a>
-        <!-- <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold">
-            <i class="fas fa-power-off me-2"></i>Logout
-        </a> -->
         <a href="?menu=kontakt" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-envelope me-2"></i>Kontakt
+            <i class="fas fa-envelope me-2"></i>
+            Kontakt
         </a>
         <a href="?menu=datenschutz" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-user-secret me-2"></i>Datenschutz
+            <i class="fas fa-user-secret me-2"></i>
+            Datenschutz
         </a>
         <a href="?menu=impressum" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-            <i class="fas fa-industry me-2"></i>Impressum
+            <i class="fas fa-industry me-2"></i>
+            Impressum
         </a>
         <!-- gehören später nach sidebar_post_login umgestellt -->
         <a href="?menu=template" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
             VORLAGE
         </a>
         <a href="?menu=dashboard" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-        <i class="fas fa-chart-area me-2"></i>Dashboard
+            <i class="fas fa-balance-scale me-2"></i>
+            Bilanz
         </a>
     </div>
 </div>
