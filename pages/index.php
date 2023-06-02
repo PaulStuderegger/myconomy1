164665bfd1f044749dfeafb66a22e71629a22171
Matthias Weingartner <toothless21799@gmail.com>
2023-06-02 08:38:28 +0200

    forms.css einfügen

--- pages/index.php
+++ pages/index.php
@@ -11,14 +11,15 @@ session_start();
 	<title>Startseite</title>
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     
     <!-- Icons - Fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     
     <!-- eigenes CSS -->
-    <link rel="stylesheet" href="../styles/fonts.css">
+    <link rel="stylesheet" href="../styles/fonts.css" />
+    <link rel="stylesheet" href="../styles/forms.css" />
     <link rel="stylesheet" href="../styles/functions.css" />
     <link rel="stylesheet" href="../styles/styles.css" />
 </head>
