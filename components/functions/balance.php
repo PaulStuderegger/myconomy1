a0731990dc33de34b66073196c44d2d8be3b1dd8
Matthias Weingartner <toothless21799@gmail.com>
2023-06-16 08:22:31 +0200

    dashboard.php zu balance.php umbenennen

--- components/functions/balance.php
+++ components/functions/balance.php
@@ -13,7 +13,7 @@ require_once "..\classes" . DIRECTORY_SEPARATOR . "transaction.php";
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
     <div class="d-flex align-items-center">
         <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-        <h2 class="fs-2 m-0">Dashboard</h2>
+        <h2 class="fs-2 m-0">Bilanz</h2>
     </div>
 </nav>
 
